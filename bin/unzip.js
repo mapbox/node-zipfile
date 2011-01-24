3d1c50f65f4e8218cf9a24b758b5b6c89f90f4f2
Dane Springmeyer <dane@dbsgeo.com>
2011-01-23 21:19:57 -0500

    memcpy include, and properly install unzip.js command line program

---
+++ bin/unzip.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var zip = require('zipfile');
+var fs = require('fs');
+var path = require('path');
+
+// node 0.2 <-> 0.3 compatibility
+var constants = {};
+if (!process.EEXIST >= 1) 
+    constants = require('constants');
+else
+    constants.EEXIST = process.EEXIST;
+
+var usage = 'usage: unzip.js <zipfile>';
+
+var file = process.ARGV[2];
+if (!file) {
+   console.log(usage);
+   process.exit(1);
+}
+
+var zf = new zip.ZipFile(file);
+
+zf.names.forEach(function(name) {
+    var uncompressed = path.join('.',name);
+    var dirname = path.dirname(uncompressed);
+    fs.mkdir(dirname, 0755 , function(err) {
+        if (err && err.errno != constants.EEXIST) throw err;
+        if (path.extname(name)) {
+            var buffer = zf.readFileSync(name);
+            fd = fs.openSync(uncompressed,'w');
+            fs.writeSync(fd, buffer, 0, buffer.length, null);
+            fs.closeSync(fd);
+        }
+    })
+})
