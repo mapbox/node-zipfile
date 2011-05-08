9625cc7b370bbb97e6a6e80c30635f5be1033170
Dane Springmeyer <dane@dbsgeo.com>
2011-05-07 18:09:39 -0700

    bump version to 0.1.8

--- src/_zipfile.cc
+++ src/_zipfile.cc
@@ -19,7 +19,7 @@ extern "C" {
     ZipFile::Initialize(target);
 
     // node-zipfile version
-    target->Set(String::NewSymbol("version"), String::New("0.1.7"));
+    target->Set(String::NewSymbol("version"), String::New("0.1.8"));
 
     // versions of deps
     Local<Object> versions = Object::New();
