2e6d07563deb3eb8f8ba98d3e96fac2abeb76786
Dane Springmeyer <dane@dbsgeo.com>
2012-04-18 11:30:25 -0700

    cross platform mkdirp

--- test/write.test.js
+++ test/write.test.js
@@ -1,17 +1,17 @@
 var zipfile = require('zipfile');
 var assert = require('assert');
-var support = require('./support/helper');
 var path = require('path');
 var fs = require('fs');
 var constants = require('constants');
+var mkdirp = require('mkdirp');
 
 describe('Async Writes', function(){
 
     var zf = new zipfile.ZipFile('./test/data/world_merc.zip');
     zf.names.forEach(function(name) {
         it('async write '+ name, function(done){
-            var dest = path.join(__dirname,'/tmp/', name);
-            support.mkdirP(path.dirname(dest), 0755 , function(err) {
+            var dest = path.join(__dirname,'tmp', name);
+            mkdirp(path.dirname(dest), 0755 , function(err) {
                 if (err) throw err;
                 if (path.extname(name)) {
                     zf.readFile(name, function(err, buffer) {
@@ -37,15 +37,12 @@ describe('Async Writes', function(){
 });
 
 describe('Sync Writes', function(){
-
     var zf = new zipfile.ZipFile('./test/data/world_merc.zip');
-    var wrote = 0;
-
-    it('sync write', function(){
-        zf.names.forEach(function(name) {
-            var uncompressed = path.join('/tmp/sync', name);
-            var dirname = path.dirname(uncompressed);
-            support.mkdirP(dirname, 0755 , function(err) {
+    zf.names.forEach(function(name) {
+        var uncompressed = path.join('/tmp/sync', name);
+        var dirname = path.dirname(uncompressed);
+        mkdirp(dirname, 0755 , function(err) {
+            it('sync write '+ name, function(done){
                 if (err && !err.message.match(/^EEXIST/)) throw err;
                 if (path.extname(name)) {
                     var buffer = zf.readFileSync(name);
@@ -53,14 +50,9 @@ describe('Sync Writes', function(){
                     fs.writeSync(fd, buffer, 0, buffer.length, null);
                     fs.closeSync(fd);
                     assert.ok(path.existsSync(uncompressed));
-                    ++wrote;
+                    done();
                 }
             });
         });
     });
-
-    after(function(){
-      assert.equal(wrote, 4);
-    });
-
 });
