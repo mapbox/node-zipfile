d7576233c3c2b75bfda2c32bad7c5bd67f6c03e1
Dane Springmeyer <dane@mapbox.com>
2014-07-31 15:11:54 -0700

    update local osx packaging script

--- scripts/package_osx.sh
+++ scripts/package_osx.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
+set -u -e
 
 CURRENT_DIR="$( cd "$( dirname $BASH_SOURCE )" && pwd )"
 cd $CURRENT_DIR/../
 source ~/.nvm/nvm.sh
+nvm install 0.10
 nvm use 0.10
-
-set -u -e
-
-npm install node-gyp
+npm install node-pre-gyp
 npm install aws-sdk
-export PATH=`pwd`/node_modules/.bin:$PATH
+./node_modules/.bin/node-pre-gyp info
+npm cache clean
+rm -rf sdk
 
-function doit {
-    rm -rf build
-    node-pre-gyp clean build package testpackage "$@"
+function doit () {
+    NVER=$1
+    nvm install $1
+    nvm use $1
+    npm install --build-from-source
     npm test
-    ./node_modules/.bin/node-pre-gyp publish info "$@"
+    node ./node_modules/.bin/node-pre-gyp package testpackage
+    node ./node_modules/.bin/node-pre-gyp testpackage
+    npm ls
+    node ./node_modules/.bin/node-pre-gyp publish
+    node ./node_modules/.bin/node-pre-gyp info
+    rm -rf {build,lib/binding}
     npm install --fallback-to-build=false
     npm test
 }
 
-doit
-nvm use 0.8
-doit --target=0.8.26
+doit 0.8
+doit 0.10
+doit 0.11.13
+
+# to avoid then publishing with node v0.11.x
+# https://github.com/npm/npm/issues/5515#issuecomment-46688278
+nvm use 0.10
