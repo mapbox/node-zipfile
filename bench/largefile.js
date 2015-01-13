389c782f06781a2e69423ba23212d56933dca11c
Dane Springmeyer <dane@mapbox.com>
2015-01-13 14:03:23 -0800

    benchmark times for each step

--- bench/largefile.js
+++ bench/largefile.js
@@ -24,13 +24,18 @@ if (!fs.existsSync(filepath)) {
 function unzip(err) {
     if (err) throw err;
     console.log('Unzipping...');
+    console.time('opening')
     var zf = new zipfile.ZipFile(filepath);
+    console.timeEnd('opening')
+    console.time('copying dbf')
     zf.copyFileSync('US_OG_022014.dbf', 'out.dbf');
+    console.timeEnd('copying dbf')
     validate('out.dbf');
 }
 
 function validate(filename) {
     var s = fs.ReadStream(filename);
+    console.time('validating')
     s.on('data', function(d) {
       shasum.update(d);
     });
@@ -39,6 +44,7 @@ function validate(filename) {
       var d = shasum.digest('hex');
       console.log(d + '  ' + filename);
       assert.equal(d,expected_md5);
+      console.timeEnd('validating')
     });
 }
 
